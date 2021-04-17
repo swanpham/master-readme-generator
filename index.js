a9cc9095cdd3ae54fdc07f6a6ed6f60a6d329434
Swan Pham <accounting@nailsrus.ca>
2021-04-17 19:11:21 -0400

    add question

--- index.js
+++ index.js
@@ -12,24 +12,96 @@ function init() {}
 // Function call to initialize app
 init();
 
+const inquirer = require('inquirer');
 
-const fs = require('fs');
 
-const generatePage = require('./src/page.template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page.template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
+//   console.log('Master READme Generator complete! Check out index.html to see the output!');
+// });
 
-console.log(name, github);
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub URL'
+      },
+      {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter your email address'
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
+      }
+    ]);
+};
+const promptProject = () => {
+console.log(`
+=================
+Add a New Project
+=================
+`);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ]);
+};  
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
 
-const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
 
-  console.log('Master Readme Generator complete! Check out index.html to see the output!');
-});
 
